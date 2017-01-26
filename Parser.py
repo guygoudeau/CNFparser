430439e46ff8b3a5d5256d8a3cb43629204fb471
Guy Goudeau <guy.goudeau@students.aie.edu.au>
2017-01-26 13:29:50 -0600

    changed it up

--- Parser.py
+++ Parser.py
@@ -1,44 +1,54 @@
-print("& - and");
-print("| - or");
-print("! - not");
-print("( - begin clause");
-print(") - end clause\n");
+print("\n& - and")
+print("| - or")
+print("! - not")
+print("( - begin clause")
+print(") - end clause\n")
+print("abcdef = 101010\n")
 
-fileName = "expressions.txt";
-inFile = open(fileName, 'r').read().splitlines();
+a = 1
+b = 0
+c = 1
+d = 0
+e = 1
+f = 0
+
+fileName = "expressions.txt"
+inFile = open(fileName, 'r').read().splitlines()
+
 for line in inFile:
-	literals = [];
-	numOfLiterals = 0;
-	numOfClauses = 0;
-	openClause = 0;
+
+	literals = []
+	currentClause = []
+	evaledClause = []
+	numOfLiterals = 0
+	numOfClauses = 0
 		
 	for char in line:
 		if char == '(':
-			openClause = 1;
+			numOfClauses += 1
+			currentClause = []
+			evaledClause = []
 			
-		elif char == ')' and openClause == 1:
-			numOfClauses += 1;
-			openClause = 0;
-				
-		elif char == '&':
-			continue;
-			
-		elif char == '!':
-			continue;
-			
-		elif char == '|':
-			continue;
-			
-		elif char == '\n':
-			continue;
-				
-		else: 
-			if char in literals:
-				continue;
-			literals.append(char);
+		elif char != ')':
+			currentClause.append(char)
+			if line[char - 1] == "!":
+				if eval(char) == 1:
+					evaledClause.append(0)
+				else:
+					evaledClause.append(1)
+			else:
+				if char.isalpha():
+					evaledClause.append(char)
+					
+		else:
+			currentClause.append(char)
+					 
+		if char in literals:
+			continue
+		literals.append(char)
 	
-	print line;
-	print "Literals: ", literals;
-	print "Number of literals: ", len(literals);
-	print "Number of clauses: ", numOfClauses;
-	print "";
+	print line
+	print "Literals: ", literals
+	print "Number of literals: ", len(literals)
+	print "Number of clauses: ", numOfClauses
+	print ""
